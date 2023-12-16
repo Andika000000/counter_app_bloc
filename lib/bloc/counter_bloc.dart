f2378a1979025bf5f8ab4b9e5810c6a20ad596ae
Akbar_Rahmadani_2141762150 <119948941+AkbarRahmadani161102@users.noreply.github.com>
2023-12-16 21:23:36 +0700

    first commit

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,23 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
+import 'package:meta/meta.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterState(counterValue: 0)) {
+    on<CounterEvent>((event, emit) {
+      // TODO: implement event handler
+      if (event is Increment) {
+        emit(CounterState(counterValue: state.counterValue + 1));
+      }
+      if (event is Decrement) {
+        if (state.counterValue > 0) {
+          emit(CounterState(counterValue: state.counterValue - 1));
+        }
+      }
+    });
+  }
+}
