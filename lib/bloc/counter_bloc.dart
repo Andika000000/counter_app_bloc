0b4e236d9aaee86fcac5131d542f6a274e84575b
Muhammad Andika <143993759+Andika000000@users.noreply.github.com>
2023-12-20 08:35:46 +0700

    edit

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,6 +1,5 @@
-// ignore: depend_on_referenced_packages
+
 import 'package:bloc/bloc.dart';
-// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 
 part 'counter_event.dart';
